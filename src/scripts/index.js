da622bc718fa337296f8ddc663f23c7e773366d7
Li7252 <lidiya.yasyrckina@gmail.com>
2023-08-19 00:28:02 +0300

    feat(header): add header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 import('../pages/index.css');
 
 console.log('HedonismFest');
+
+const dropdownMenu = document.querySelector('.header__menu-list');
+
+function openDropdown() {
+  dropdownMenu.classList.toggle('header__menu-list_show');
+}
+
+const buttonDropdown = document.querySelector('#button-dropdown');
+buttonDropdown.addEventListener('click', openDropdown);
+
+function closeDropdownOutside() {
+
+}
+
+window.addEventListener('mousedown', (evt) => {
+    if (dropdownMenu.classList.contains('header__menu-list_show') && evt.target !== dropdownMenu) {
+      openDropdown();
+  }
+});
