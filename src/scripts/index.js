70b2a4441852da82c1c4e5b4bb5438fa3e705db7
Li7252 <lidiya.yasyrckina@gmail.com>
2023-09-09 16:42:22 +0300

    fix(header): fix all

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,71 +5,119 @@ import('./join');
 import { renderCard } from '../components/card';
 import cards from '../data/data.json';
 import { tns } from '../../node_modules/tiny-slider';
+import { openDonateForm } from '../components/modal';
 
 console.log('HedonismFest');
 
-cards.forEach((el) => renderCard(el, '.events__slider'));
+const donateButton = document.querySelector('.header__donate-button');
+const donateButtonBurger = document.querySelector(
+	'.header__donate-button_burger'
+);
 
-const dropdownMenu = document.querySelector('.header__menu-list');
-
+// Выпадающее меню
+const dropdownMenu = document.querySelector('.header__dropdown-menu');
+const buttonDropdown = document.querySelector('.header__btn-dropdown');
 const activeCity = localStorage.getItem('city') || false;
-
-function openDropdownLocation() {
-	dropdownMenu.classList.toggle('header__menu-list_show');
-}
-
-function closeDropdownLocation() {
-	dropdownMenu.classList.remove('header__menu-list_show');
-}
-
-const buttonDropdown = document.querySelector('#button-dropdown');
-buttonDropdown.addEventListener('click', openDropdownLocation);
 const headerItems = document.querySelectorAll('.header__list-item');
+const buttonBurgerCity = document.querySelector('.header__btn-dropdown_burger');
 
+// функция открытия выпадающего меню
+function openDropdownLocation() {
+	dropdownMenu.classList.add('header__dropdown-menu_opened');
+}
+
+// функция закрытия выпадающего меню
+function closeDropdownLocation() {
+	dropdownMenu.classList.remove('header__dropdown-menu_opened');
+}
+
+// функци очистки выбранного города
 const removeActive = () => {
 	headerItems.forEach((item) => {
-		item
-			.querySelector('.header__checked-mark')
-			.classList.remove('header__checked-mark_checked');
+		if (item.children[0].value !== localStorage.getItem('city')) {
+			item
+				.querySelector('.header__checked-mark')
+				.classList.remove('header__checked-mark_checked');
+		}
 	});
 };
 
+// функция выбора города
 const setSelectedElement = (element) => {
 	removeActive();
 	element
 		.querySelector('.header__checked-mark')
-		.classList.toggle('header__checked-mark_checked');
+		.classList.add('header__checked-mark_checked');
 	buttonDropdown.querySelector('p').textContent =
+		element.querySelector('label').textContent;
+	buttonBurgerCity.querySelector('p').textContent =
 		element.querySelector('label').textContent;
 };
 
+// поиск активного города
 if (activeCity) {
 	headerItems.forEach((item) => {
-		if (item.querySelector(`#${activeCity}`)) {
+		if (item.children[0].value === activeCity) {
 			setSelectedElement(item);
 		}
 	});
 }
 
+// запись активного города в localStorage
 headerItems.forEach((item) => {
 	item.addEventListener('click', function () {
 		localStorage.setItem(
 			'city',
-			this.querySelector('input').getAttribute('id')
+			this.querySelector('input').getAttribute('value')
 		);
 		setSelectedElement(this);
 	});
 });
 
-// function closeDropdownOutside() {}
+// открыть меню по клику
+buttonDropdown.addEventListener('click', openDropdownLocation);
 
-function handleClickOutside(element, callback) {
-	function handleClick(event) {
-		if (!element.contains(event.target) && element !== event.target) {
-			callback();
-		}
+// закрытие выпадающего меню при клике вне меню
+document.addEventListener('mousedown', (evt) => {
+	if (evt.target === dropdownMenu) {
+		closeDropdownLocation();
 	}
-}
+});
+
+// Бургерное меню
+const burgerMenuContainer = document.querySelector(
+	'.header__burger-menu-container'
+);
+const burgerMenu = document.querySelector('.header__burger-menu');
+const burgerButtonBack = document.querySelector('.header__btn-back');
+const headerButtonMenu = document.querySelector('.header__btn-menu');
+
+// открытие бургерного меню
+headerButtonMenu.addEventListener('click', function () {
+	burgerMenuContainer.classList.toggle('header__burger-menu-container_opened');
+});
+
+// открытие выбора города в бургерном меню
+buttonBurgerCity.addEventListener('click', function () {
+	burgerMenu.classList.remove('header__burger-menu_close');
+	burgerMenu.classList.add('header__burger-menu_open');
+});
+
+// возвращение на главную страницу бургерного меню
+burgerButtonBack.addEventListener('click', function () {
+	burgerMenu.classList.remove('header__burger-menu_open');
+	burgerMenu.classList.add('header__burger-menu_close');
+});
+
+// открытие формы пожертвований
+donateButton.addEventListener('click', openDonateForm);
+donateButtonBurger.addEventListener('click', openDonateForm);
+
+// Слайдер
+const eventSlider = document.querySelector('.events__slider');
+
+// добавление карточек в слайдер
+if (eventSlider) cards.forEach((el) => renderCard(el, '.events__slider'));
 
 if (document.getElementById('page')) {
 	var slider = tns({
@@ -82,28 +130,3 @@ if (document.getElementById('page')) {
 		mouseDrag: true,
 	});
 }
-
-document.addEventListener('click', handleClickOutside);
-
-handleClickOutside(buttonDropdown, closeDropdownLocation);
-
-buttonDropdown.addEventListener('click', openDropdownLocation);
-
-const buttonBurgerCity = document.querySelector('.header__btn-dropdown_burger');
-const burgerMenu = document.querySelector('.header__burger-menu');
-const burgerButtonBack = document.querySelector('.header__btn-back');
-const headerButtonMenu = document.querySelector('.header__btn-menu');
-
-headerButtonMenu.addEventListener('click', function () {
-	burgerMenu.classList.toggle('header__burger-menu_opened');
-});
-
-buttonBurgerCity.addEventListener('click', function () {
-	burgerMenu.classList.remove('header__burger-menu_close');
-	burgerMenu.classList.add('header__burger-menu_open');
-});
-
-burgerButtonBack.addEventListener('click', function () {
-	burgerMenu.classList.remove('header__burger-menu_open');
-	burgerMenu.classList.add('header__burger-menu_close');
-});
